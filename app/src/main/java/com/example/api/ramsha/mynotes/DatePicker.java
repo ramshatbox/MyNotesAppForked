f1f827babd775864a483abdf991215e45799657a
RSoft456 <ramshamehmood456@gmail.com>
2023-06-02 16:36:34 +0500

    sign up page designed

---
+++ app/src/main/java/com/example/api/ramsha/mynotes/DatePicker.java
@@ -0,0 +1,11 @@
+package com.example.api.ramsha.mynotes;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
